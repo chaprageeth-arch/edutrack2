511ddbf42968acaa7950bf746b340de3736668d0
chaprageeth-arch <chaprageeth@gmail.com>
2026-06-28 19:52:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Welcome!");
+// Cursor follow effect
+const cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", (e) => {
+cursor.style.left = e.pageX + "px";
+cursor.style.top = e.pageY + "px";
+});
+
+// Smooth scroll feel
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+anchor.addEventListener("click", function (e) {
+e.preventDefault();
+document.querySelector(this.getAttribute("href")).scrollIntoView({
+behavior: "smooth"
+});
+});
+});
